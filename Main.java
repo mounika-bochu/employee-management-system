b94a9f5c2856c94bf646eaf65f2f9df82c241ec4
bochu-mounika <mounikayadav0515@gmail.com>
2026-05-24 16:45:58 +0530

    Added payment feature

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Management System");
+        System.out.println("Payment Feature Addedd");
     }
 }
